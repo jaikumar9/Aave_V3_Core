297c2fead35849b5694917ee3e44d50336141ba0
jaikumar9 <jaiagdayemawer556@gmail.com>
2024-02-28 12:21:55 +0530

    AAve Learning First Commit

---
+++ contracts/protocol/pool/Pool.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.10;
+
+import {VersionedInitializable} from '../libraries/aave-upgradeability/VersionedInitializable.sol';
+import {Errors} from '../libraries/helpers/Errors.sol';
+import {ReserveConfiguration} from '../libraries/configuration/ReserveConfiguration.sol';
+import {PoolLogic} from '../libraries/logic/PoolLogic.sol';
+import {ReserveLogic} from '../libraries/logic/ReserveLogic.sol';  // this lib we are using in this contract 
+import {EModeLogic} from '../libraries/logic/EModeLogic.sol';
+import {SupplyLogic} from '../libraries/logic/SupplyLogic.sol';
+import {FlashLoanLogic} from '../libraries/logic/FlashLoanLogic.sol';
+import {BorrowLogic} from '../libraries/logic/BorrowLogic.sol';
+import {LiquidationLogic} from '../libraries/logic/LiquidationLogic.sol';
+import {DataTypes} from '../libraries/types/DataTypes.sol';
+import {BridgeLogic} from '../libraries/logic/BridgeLogic.sol';
+import {IERC20WithPermit} from '../../interfaces/IERC20WithPermit.sol';
+import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
+import {IPool} from '../../interfaces/IPool.sol';
+import {IACLManager} from '../../interfaces/IACLManager.sol';
+import {PoolStorage} from './PoolStorage.sol';
+
+
+/**
+ * @title Pool contract
+ * @author Aave
+ * @notice Main point of interaction with an Aave protocol's market
+ * - Users can:
+ *   # Supply
+ *   # Withdraw
+ *   # Borrow
+ *   # Repay
+ *   # Swap their loans between variable and stable rate
+ *   # Enable/disable their supplied assets as collateral rebalance stable rate borrow positions
+ *   # Liquidate positions
+ *   # Execute Flash Loans
+ * @dev To be covered by a proxy contract, owned by the PoolAddressesProvider of the specific market
+ * @dev All admin functions are callable by the PoolConfigurator contract defined also in the
+ *   PoolAddressesProvider
+ */
+
+
+contract Pool is VersionedInitializable, PoolStorage, IPool  {
+  using ReserveLogic for DataTypes.ReserveData;
+
+  uint256 public constant POOL_REVISION = 0x1;
+  IPoolAddressesProvider public immutable ADDRESSES_PROVIDER;
+
+/**
+   * @dev Only pool configurator can call functions marked by this modifier.
+   */
+
+  
+
+
+
+}
